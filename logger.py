6c50b5629d24faa1b36de5257cadcdcced20324b
51j0 <sijoabraham8@gmail.com>
2020-06-27 01:11:33 +0800

    logger file

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='myapp.log',
+                    filemode='w')
+
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
