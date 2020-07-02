5f28d4547bea068b4d473df4ab5ce5f2c0017d44
51j0 <sijoabraham8@gmail.com>
2020-07-03 00:24:12 +0800

    code cleanup

--- main.py
+++ main.py
@@ -8,24 +8,75 @@ parser = optparse.OptionParser()
 parser.add_option('-i','--install',dest="install",default="none")
 parser.add_option('-r','--uninstall',dest="uninstall",default="none")
 parser.add_option('-d','--decompile',dest="decompile",default="none")
+parser.add_option('-f','--file',dest="file",default="none")
 parser.add_option('-c','--compile', dest="compile",default="none")
 parser.add_option('-p','--pull', dest="fetch",default="none")
 parser.add_option('-s','--sign', dest="sign",default="none")
-parser.add_option('-f','--find', dest="find",default="none")
+parser.add_option('-l','--look', dest="look",default="none")
+parser.add_option('-b','--bypass', dest="bypass",default="none")
 parser.add_option('-u','--unlock',dest="unlock",action="store",default="none")
 #parser.add_option('-h','--help', default="auto")
 options, remainder = parser.parse_args()
 
 
 
-if options.install != 'none':
+
+if options.bypass != 'none':
+    if 'sslpinning' in options.bypass or 'debugging' in options.bypass:
+
+        value = options.file
+        if(len(value)==0):
+            print("Invalid Option")
+            exit()
+
+        folder = "";
+        if value.endswith('.apk'):
+            apktool = Apktool(value)
+            apktool.decompile()
+            folder = value.replace(".apk")
+        elif '.' in value:
+            print("Looking for %s in your device"%value)
+            adb = Adb(value)
+            adb.fetchApk(".")
+            print("Fetching %s from device"%adb.packagename())
+            apktool = Apktool("base.apk")
+            apktool.decompile()
+            folder = "base"
+        else:
+            bypass = Bypass(value)
+            if(not bypass.validFolder()):
+                print("Invalid Option")
+                exit()
+
+            folder = value
+
+        if options.bypass == 'sslpinning':
+            bypass = Bypass(folder)
+            bypass.udpateNetworkSecurityConfig()
+
+        if options.bypass == 'debugging':
+            bypass = Bypass(folder)
+            bypass.mainfestdebuggable()
+
+        apktool = Apktool(folder)
+        apktool.compile()
+        apktool = Apktool("%s/dist/%s.apk"%(value,value))
+        apktool.sign()
+
+        text = input("Would you like to install the apk (y/N): ")
+        if text == 'y' or text == "Y":
+            adb = Adb("%s/dist/%s.apk"%(value,value))
+            print(adb.install())
+    else:
+        print("Unknown bypass option")
+elif options.install != 'none':
     adb = Adb(options.install)
     print(adb.install())
-if options.uninstall != 'none':
+elif options.uninstall != 'none':
     adb = Adb(value)
     print(adb.uninstall())
-elif options.find != 'none':
-    value = options.find
+elif options.look != 'none':
+    value = options.look
     adb = Adb(value)
     name = adb.packagename()
     path = adb.getPackagePath()
@@ -58,10 +109,6 @@ elif options.decompile != "none":
 elif options.sign != "none":
     apktool = Apktool(options.sign)
     print(apktool.sign())
-    
-
-
-
 
 else:
     print("Unknown Option")
